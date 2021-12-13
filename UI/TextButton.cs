746c54809bb3430210c56be94583a6d26056b079
Brian Cable <brian.cable.dev@gmail.com>
2021-12-13 08:20:51 -0600

    initial text button

---
+++ UI/TextButton.cs
@@ -0,0 +1,40 @@
+﻿using Backbone.Graphics;
+using Backbone.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backbone.UI
+{
+    public class TextButton : IGUI3D
+    {
+        private TextGroup Text;
+
+        public TextButton(TextButtonSettings settings)
+        {
+            Text = new TextGroup(settings);
+        }
+
+        public void HandleMouse(HandleMouseCommand command)
+        {
+        }
+
+        public void TransitionIn()
+        {
+        }
+
+        public void TransitionOut()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+        }
+
+    }
+}
---
+++ UI/TextButtonSettings.cs
@@ -0,0 +1,18 @@
+﻿using Backbone.Graphics;
+using Backbone.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backbone.UI
+{
+    public class TextButtonSettings<T>
+    {
+        public TextGroupSettings TextGroupSettings { get; set; }
+        public InputAction AssignedKey { get; set; }
+
+        public T RaisedEventOnClick { get; set; }
+
+    }
+}
