0074b78f3d231d3342ae8bbb73bc39137283c967
Brian Cable <brian.cable.dev@gmail.com>
2022-07-26 08:05:35 -0500

    Add text align type.

---
+++ UI/TextAlign.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backbone.UI
+{
+    public enum TextAlign
+    {
+        Left,
+        Center,
+        Right
+    }
+}
