fc13e8f12d4f1811b49f69804c809ade1f008362
Brian Cable <brian.cable.dev@gmail.com>
2023-01-25 14:18:33 -0600

    Initial Chat scaffolding based on an old Twitch chat example project I made a long time ago. Probably pretty broken right now.

---
+++ Chat/TCPChatSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backbone.Chat
+{
+    public class TCPChatSettings
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public string Password { get; set; }
+
+        // username for the bot user (for Twitch) or just the nickname for the IRC chat
+        public string Nick { get; set; }
+    }
+}
