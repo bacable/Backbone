0947c470cb67ea33e9ce91f1d59fec93ea6b0663
Brian Cable <brian.cable.dev@gmail.com>
2023-02-06 08:05:17 -0600

    Minor start to Console Panel

---
+++ UI/ConsolePanel.cs
@@ -0,0 +1,32 @@
+﻿using Backbone.Graphics;
+using Backbone.Input;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Backbone.UI
+{
+    public class ConsolePanel : IGUI3D
+    {
+        public List<ConsoleItem> Items = new List<ConsoleItem>();
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+        }
+
+        public void HandleMouse(HandleMouseCommand command)
+        {
+        }
+
+        public void TransitionIn()
+        {
+        }
+
+        public void TransitionOut()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+    }
+}
