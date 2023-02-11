46fd9240ff22bc4bca112dca43e5cab78da4d226
Brian Cable <brian.cable.dev@gmail.com>
2023-02-11 10:35:09 -0600

    minor changes

--- UI/ConsolePanel.cs
+++ UI/ConsolePanel.cs
@@ -2,6 +2,8 @@
 using Backbone.Graphics;
 using Backbone.Input;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 
 namespace Backbone.UI
@@ -10,15 +12,52 @@ namespace Backbone.UI
     {
         public List<ConsoleItem> Items = new List<ConsoleItem>();
 
+        public ConsolePanel()
+        {
+            AddExampleLog();
+        }
+
+        public void AddExampleLog()
+        {
+            Items.Add(new ConsoleItem()
+            {
+                Message = "votes to play at A4",
+                User = "RandomUser1",
+                StartDate = DateTime.Now,
+                UserColor = Color.Blue
+            });
+            Items.Add(new ConsoleItem()
+            {
+                Message = "votes to play at G7",
+                User = "RandomUser88",
+                StartDate = DateTime.Now,
+                UserColor = Color.Purple
+            });
+            Items.Add(new ConsoleItem()
+            {
+                Message = "votes to play at N5",
+                User = "RandomUser3",
+                StartDate = DateTime.Now,
+                UserColor = Color.Navy
+            });
+        }
+
         public void Draw(Matrix view, Matrix projection)
         {
         }
+
+        public void DrawText(SpriteBatch spriteBatch)
+        {
+
+        }
+
         public void HandleEvent(ConsolePanelEvent eventType, object payload)
         {
             switch(eventType)
             {
                 case ConsolePanelEvent.AddLine:
-
+                    Items.Add((ConsoleItem)payload); break;
+                default: break;
             }
         }
         public void HandleMouse(HandleMouseCommand command)
