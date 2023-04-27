79ad7871ffb92f46a5bbcda9f96e840e5d9966b7
Brian Cable <brian.cable.dev@gmail.com>
2023-04-27 08:03:33 -0500

    Start to shift how colors handled so more flexible.

---
+++ Graphics/ColorHex.cs
@@ -0,0 +1,28 @@
+﻿using Backbone.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProximityND.Backbone.Graphics
+{
+    public class ColorHex
+    {
+        private static Dictionary<string, Color> Colors = new Dictionary<string, Color>();
+
+        private static Color Get(string hexCode)
+        {
+            if(Colors.ContainsKey(hexCode)) {
+                return Colors[hexCode];
+            } else
+            {
+                var color = System.Drawing.ColorTranslator.FromHtml(hexCode);
+                Colors[hexCode] = color;
+                return color;
+            }
+        }
+
+    }
+}
