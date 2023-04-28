3f55208033b72fc61b75426abf1599f43fcca4b1
Brian Cable <brian.cable.dev@gmail.com>
2023-04-28 08:20:23 -0500

    MenuCard interface for components that act like visual cards in menu items

---
+++ Graphics/IMenuCard.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backbone.Graphics
+{
+    public interface IMenuCard
+    {
+        public int ID { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }        
+        public string Title { get; set; }
+        void Draw(Matrix view, Matrix projection);
+        void SetData(object data);
+        void Update(GameTime gameTime);
+    }
+}
