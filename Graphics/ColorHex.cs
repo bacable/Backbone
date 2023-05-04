3ff9c3d4b0ca869c9ee0f5cf37aa7d8d33b37593
Brian Cable <brian.cable.dev@gmail.com>
2023-05-03 20:00:31 -0500

    Added conversion from hex code to XNA color.

--- Graphics/ColorHex.cs
+++ Graphics/ColorHex.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 
@@ -38,6 +39,17 @@ namespace Backbone.Graphics
                 Colors[hexCode] = colorVector;
                 return colorVector;
             }
+        }
+
+        public static Microsoft.Xna.Framework.Color ConvertFromHex(string s)
+        {
+            if (s.Length != 7)
+                return Microsoft.Xna.Framework.Color.Gray;
+
+            int r = Convert.ToInt32(s.Substring(1, 2), 16);
+            int g = Convert.ToInt32(s.Substring(3, 2), 16);
+            int b = Convert.ToInt32(s.Substring(5, 2), 16);
+            return new Microsoft.Xna.Framework.Color(r, g, b);
         }
 
     }
