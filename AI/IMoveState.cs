c99a179a059b010de7cd42e9b9c10e8bea4fea30
Brian Cable <brian.cable.dev@gmail.com>
2023-10-02 08:32:05 -0500

    Initial work to add support for AI.

---
+++ AI/IMoveState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProximityND.Backbone.AI
+{
+    public interface IMoveState
+    {
+        IGameState GameState { get; set; }
+        string Move { get; set; }
+        float Score { get; set; }
+    }
+}
