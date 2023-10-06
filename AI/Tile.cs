a3210b419b16c97489bb7cb6fec7359e215581af
Brian Cable <brian.cable.dev@gmail.com>
2023-10-06 08:22:34 -0500

    initialize dict for ai tile

--- AI/Tile.cs
+++ AI/Tile.cs
@@ -9,7 +9,7 @@ namespace Backbone.AI
     public class Tile
     {
         public string ID { get; set; }
-        public Dictionary<string, object> Properties { get; set; }
+        public Dictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
 
         public object Get(string key) { return Properties[key]; }
         public void Set(string key, object value) { Properties[key] = value; }
