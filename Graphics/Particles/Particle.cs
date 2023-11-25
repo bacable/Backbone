9aa1d06923a05829be11e38c615b3c5a2edcc052
Brian Cable <brian.cable.dev@gmail.com>
2023-11-25 12:27:44 -0600

    New particle emitter.

---
+++ Graphics/Particles/Particle.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Backbone.Graphics.Particles
+{
+    public class Particle
+    {
+        public Model Model;
+        public Matrix World;
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public Vector3 AngularVelocity;
+        public float Life;
+        public float Gravity;
+        public float Alpha; // Transparency component
+        private float Alive = 0;
+
+        public Particle(Model model, Vector3 position, Vector3 velocity, Vector3 angularVelocity, float life, float gravity)
+        {
+            Model = model;
+            Position = position;
+            Velocity = velocity;
+            AngularVelocity = angularVelocity;
+            Life = life;
+            Gravity = gravity;
+            Alpha = 1.0f; // Fully opaque initially
+            UpdateWorldMatrix();
+        }
+
+        private void UpdateWorldMatrix()
+        {
+            World = Matrix.CreateTranslation(Position);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            Alive += elapsed;
+
+            // Update particle position based on velocity and gravity
+            Position += Velocity * elapsed;
+            Velocity += Vector3.Down * Gravity * elapsed;
+
+            // Update the world matrix
+            UpdateWorldMatrix();
+
+            // Update particle rotation based on angular velocity
+            float rotationAmount = AngularVelocity.Length() * elapsed;
+            if (rotationAmount > 0.0f)
+            {
+                AngularVelocity.Normalize();
+                float rotationSpeed = rotationAmount / elapsed;
+                Quaternion rotation = Quaternion.CreateFromAxisAngle(AngularVelocity, rotationSpeed * Alive);
+                World = Matrix.CreateFromQuaternion(rotation) * Matrix.CreateTranslation(Position);
+            }
+
+            // Update particle life
+            Life -= elapsed;
+
+            // Update particle transparency
+            Alpha = MathHelper.Clamp(Life / 0.4f, 0.0f, 1.0f); // Adjust the divisor for the desired fade-out duration
+
+        }
+    }
+
+}
---
+++ Graphics/Particles/ParticleEmitter.cs
@@ -0,0 +1,82 @@
+﻿using Backbone.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Backbone.Graphics.Particles
+{
+    public class ParticleEmitter : IGUI3D
+    {
+        private List<Particle> particles;
+        private Model particleModel;
+        private float particleLife;
+        private float particleGravity;
+        private GraphicsDevice graphicsDevice;
+
+        public ParticleEmitter(GraphicsDevice graphics, Model particleModel, float particleLife, float particleGravity)
+        {
+            this.graphicsDevice = graphics;
+            this.particleModel = particleModel;
+            this.particleLife = particleLife;
+            this.particleGravity = particleGravity;
+            particles = new List<Particle>();
+        }
+
+        public void Emit(Vector3 position, Vector3 velocity, Vector3 angularVelocity, int count)
+        {
+            position.Y = graphicsDevice.Viewport.Height - position.Y;
+
+            for (int i = 0; i < count; i++)
+            {
+                particles.Add(new Particle(particleModel, position, velocity, angularVelocity, particleLife, particleGravity));
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            for (int i = particles.Count - 1; i >= 0; i--)
+            {
+                particles[i].Update(gameTime);
+
+                // Remove dead particles
+                if (particles[i].Life <= 0)
+                {
+                    particles.RemoveAt(i);
+                }
+            }
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            foreach (var particle in particles)
+            {
+                // Draw each particle
+                foreach (var mesh in particle.Model.Meshes)
+                {
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.World = particle.World;
+                        effect.View = view;
+                        effect.Projection = projection;
+                        effect.Alpha = particle.Alpha;
+                    }
+                    mesh.Draw();
+                }
+            }
+        }
+
+        public void HandleMouse(HandleMouseCommand command)
+        {
+        }
+
+        public void TransitionIn()
+        {
+            // shouldn't ever need anything here, won't be anything onscreen to transition
+        }
+
+        public void TransitionOut()
+        {
+            // might need something here if still on the screen
+        }
+    }
+}
